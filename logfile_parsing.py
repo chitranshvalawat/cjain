b2beaaff19de914fbbba2237e4da654840d78629
Chitransh Jain <chitransh.valawat@gmail.com>
2017-08-11 15:50:10 +0530

    Update logfile_parsing.py

--- logfile_parsing.py
+++ logfile_parsing.py
@@ -11,15 +11,21 @@
 ##################################################################
 
 import csv
+import glob
+import os
 
-class Result:
-
+def delete_csv():
+    for file in glob.glob("*.csv"):
+        print(file)
+        os.remove(file)
+            
+class Result_C_based:
     def __init__(self):
         self.total_count=0
         self.fail_count=0
         self.pass_count=0
     
-    def det_pass_fail(self, filename):
+    def determine_pass_fail(self, filename):
         list1=filename.split(".",1)
         csv_filename=list1[0]+'.csv'
         GeneratedFile = open(csv_filename, 'w')
@@ -61,13 +67,74 @@ class Result:
         GeneratedFile.write('\n,Fail')
         GeneratedFile.write(','+str(self.fail_count))
 
-def main():
-    log1=Result()
-    log2=Result()
-    log3=Result()
-    
-    log1.det_pass_fail("srio_test_app_log_mode0_VB0.txt")
-    log2.det_pass_fail("srio_test_app_log_mode0_VB1.txt")
-    log3.det_pass_fail("srio_test_app_log_mode0_VB2.txt")    
+class Result_T32_based:
+    def __init__(self):
+        self.total_count=0
+        self.fail_count=0
+        self.pass_count=0
 
+    def csv_heading(self, GeneratedFile):
+        GeneratedFile.write('\nS.N.')
+        GeneratedFile.write(',Test Case')
+        GeneratedFile.write(',Result')
+        GeneratedFile.write('\n')
+
+    def determine_pass_fail(self, filename, GeneratedFile):
+            
+        file = open(filename, "r")
+        lines = file.readlines()
+        file.close()
+        fail=0
+        for line in lines:
+            if line.find( "-- TEST CASE -" )!= -1:
+                #print(line)
+                line = line.replace('\r','').replace('\n','').replace('"','').replace("-- ",'')
+                GeneratedFile.write(str(self.total_count+1))
+                GeneratedFile.write(','+str(line))
+                if fail != 0:
+                    self.fail_count=self.fail_count+1
+                    #print("Failed")
+                    GeneratedFile.write(',Failed')
+                else:
+                    self.pass_count=self.pass_count+1
+                    #print("Passed")
+                    GeneratedFile.write(',Passed')
+
+                self.total_count=self.total_count+1
+                fail=0
+                GeneratedFile.write('\n')
+            
+            if line.find( "+++ TEST CASE FAILED +++" )!= -1:
+                fail=fail+1
+
+    def summery(self, GeneratedFile):
+        GeneratedFile.write('\n\n,Total')
+        GeneratedFile.write(','+str(self.total_count))
+        GeneratedFile.write('\n,Pass')
+        GeneratedFile.write(','+str(self.pass_count))
+        GeneratedFile.write('\n,Fail')
+        GeneratedFile.write(','+str(self.fail_count))
+        
+
+
+def main():
+    log1=Result_C_based()
+    log2=Result_T32_based()
+    
+
+    delete_csv()
+    
+    for file in glob.glob("*.txt"):
+        log1.determine_pass_fail(file)
+        print(file)
+
+    GeneratedFile = open("T32_summery.csv", 'a')
+    log2.csv_heading(GeneratedFile)
+    
+    for file in glob.glob("*.log"):
+        log2.determine_pass_fail(file, GeneratedFile)
+        print(file)
+        
+    log2.summery(GeneratedFile)
+    
 main()
